mirror-weekly/apigateway

commit 9ce73c08fdcbbd64f8e6620efde877c987f22034

fix(route): create reverseProxy for each request to avoid racing condition
(cherry picked from commit ac2484368acf0f160611434c9419f1bd78849109)

# Conflicts:
#	server/route.go

server/route.go
@@ -267,9 +267,6 @@ func NewSingleHostReverseProxy(target *url.URL, pathBaseToStrip string, rdb Redi
 			req.Header.Set("User-Agent", "")
 		}
 	}
-
-	reverseProxy := &httputil.ReverseProxy{Director: director}
-
 	return func(c *gin.Context) {
 		// TODO refactor modification and cache code
 		var tokenState string
@@ -309,7 +306,8 @@ func NewSingleHostReverseProxy(target *url.URL, pathBaseToStrip string, rdb Redi
 			return
 		}
 
-		reverseProxy.ModifyResponse = ModifyReverseProxyResponse(c, rdb, cacheTTL)
+		reverseProxy := httputil.ReverseProxy{Director: director}
+		reverseProxy.ModifyResponse = ModifyReverseProxyResponse(c)
 		reverseProxy.ServeHTTP(c.Writer, c.Request)
 	}
 }

commit dad9af33ac1f64e9064b2561a9f0f046c3897324

fix(route): a regression bug introduced in 9ce73c08

server/route.go
@@ -307,7 +307,7 @@ func NewSingleHostReverseProxy(target *url.URL, pathBaseToStrip string, rdb Redi
 		}
 
 		reverseProxy := httputil.ReverseProxy{Director: director}
-		reverseProxy.ModifyResponse = ModifyReverseProxyResponse(c)
+		reverseProxy.ModifyResponse = ModifyReverseProxyResponse(c, rdb, cacheTTL)
 		reverseProxy.ServeHTTP(c.Writer, c.Request)
 	}
 }